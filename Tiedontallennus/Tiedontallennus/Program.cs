7e2837b0636963a30a59fd6e5a8f726b35e6f2ad
Sami Kling <sami.kling@stud.pointcollege.fi>
2018-10-18 20:22:18 +0300

    Toteutettu lämpötilan asetus ja tallennus tiedostoon.

--- Tiedontallennus/Tiedontallennus/Program.cs
+++ Tiedontallennus/Tiedontallennus/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Tiedontallennus
 {
@@ -10,6 +11,18 @@ namespace Tiedontallennus
     {
         static void Main(string[] args)
         {
+            int lämpötila = 0;
+            Console.Write("Anna uusi lämpötila: ");
+            string syöte = Console.ReadLine();
+            lämpötila = int.Parse(syöte);
+
+            //tiedostoon kirjoittaminen
+
+            string tiedosto = @"C:\Temp\Lämpötila.txt";
+            File.WriteAllText(tiedosto, lämpötila.ToString());
+            Console.WriteLine("Uusi lämpötila on asetettu.");
+
+            Console.ReadLine();
         }
     }
 }
